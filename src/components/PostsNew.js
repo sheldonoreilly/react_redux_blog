dbef235bf58ea575be2a26fb547d30d088b0720f
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-05 23:44:54 -0230

    set the add new posts redux form

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
 
-export default class PostsNew extends Component {
+import { Field, reduxForm } from "redux-form";
+
+class PostsNew extends Component {
+	renderField(field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input className="form-control" type="text" {...field.input} />
+			</div>
+		);
+	}
+
 	render() {
-		return <div>Post New!</div>;
+		return (
+			<form>
+				<Field name="title" label="Title" component={this.renderField} />
+				<Field name="tags" label="Tags" component={this.renderField} />
+				<Field name="content" label="Post Content" component={this.renderField} />
+			</form>
+		);
 	}
 }
+
+// fields are react components, reduxForm is a
+// function somewhat similar to react-redux connect
+// allows redux form to communicate directly from the component
+// to our formReducer
+export default reduxForm({
+	form: "PostsNewForm"
+})(PostsNew);
